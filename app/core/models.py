6cfe1b3a7b4fcdd5e4809454ea2fea4294bcf7f3
pyagmyrov <permanyagmyrov@gmail.com>
2020-08-25 11:16:26 +0300

    Tested with flake8

--- app/core/models.py
+++ app/core/models.py
@@ -9,9 +9,9 @@ class UserManager(BaseUserManager):
         """Creates and saves new user"""
         if not email:
             raise ValueError('Users must have an email adress')
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -20,17 +20,17 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """test creating a new user with email is successful"""
         email = 'perman@yagmyrov.me'
         password = 'laakamo03052000'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
